6c23ec9a64cb356abf5f3459aff1c1a38d1b687e
Ivano Bruno <ivanobruno1@gmail.com>
2022-10-10 16:39:23 +0200

    General fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,46 @@
-//General
-const body = document.body;
-const h1 = document.createElement("h1");
-h1.innerHTML = "Progetto JavaScript Basic";
-body.append(h1);
-//container
-const container = document.createElement("div");
-container.classList.add("container");
-body.append(container);
-//display
-const display = document.createElement("div");
-display.classList.add("display");
-container.append(display);
-//number
-const number = document.createElement("p");
-number.innerHTML = 0;
-display.append(number);
-//Button Container
-const buttonContainer = document.createElement("div");
-buttonContainer.classList.add("button-container");
-container.append(buttonContainer);
-//button Increase
-const buttonIncrease = document.createElement("button");
-buttonIncrease.classList.add("button-increase");
-buttonIncrease.innerHTML = "+";
-buttonContainer.append(buttonIncrease);
-//button Reset
-const buttonReset = document.createElement("button");
-buttonReset.classList.add("button-reset");
-buttonReset.innerHTML = "Reset";
-buttonContainer.append(buttonReset);
-//button Decrease
-const buttonDecrease = document.createElement("button");
-buttonDecrease.classList.add("button-decrease");
-buttonDecrease.innerHTML = "-";
-buttonContainer.append(buttonDecrease);
-//footer
-const footer = document.createElement("footer");
-body.append(footer);
-const p = document.createElement("p");
-p.innerHTML = `Made with <span>❤</span> by Ivano Bruno`;
-footer.append(p);
-//counter
+//Function for create element 
+const div = document.querySelector("div");
+const createElement = (tag, text, classname, parent) => {
+    const element = document.createElement(tag);
+    element.innerHTML = text;
+    element.classList.add(classname);
+    parent.append(element);
+    return element;
+};
+//Element
+const h1 = createElement("h1", "Progetto JavaScript Basic", "title", document.body);
+const container = createElement("div", "", "container", document.body);
+const display = createElement("div", "", "display", container);
+const number = createElement("p", "0", "number", display);
+const buttonContainer = createElement("div", "", "button-container", container);
+const buttonIncrease = createElement("button", "+", "button-increase", buttonContainer);
+const buttonReset = createElement("button", "Reset", "button-reset", buttonContainer);
+const buttonDecrease = createElement("button", "-", "button-decrease", buttonContainer);
+const footer = createElement("footer", "", "footer-info", document.body);
+const p = createElement("p", `Made with <span>❤</span> by Ivano Bruno`, "bio", footer);
+//Counter
+const button = document.querySelector(".button-container");
 let i = 0;
-buttonIncrease.addEventListener("click", ()=>{
-    i++
-    if (i>0){
-        number.style.color="#008000";
-    }
-    if (i == 0){
+button.addEventListener("click", function (num) {
+    if (num.target.matches(".button-increase")) {
+        i++;
+        if (i>0){
+            number.style.color="#008000";
+        } else if (i == 0){
+            number.style.color="#000000";
+        };
+        number.innerHTML = i;
+    } else if (num.target.matches(".button-reset")){
+        i = 0;
         number.style.color="#000000";
-    }
-    number.innerHTML = i;
-})
-buttonReset.addEventListener("click", ()=>{
-    i = 0
-    number.style.color="#000000";
-    number.innerHTML = i;
-})
-buttonDecrease.addEventListener("click", ()=>{
-    i--
-    if (i<0){
-        number.style.color="#FF0000";
-    }
-    if (i == 0){
-        number.style.color="#000000";
-    }
-    number.innerHTML = i;
+        number.innerHTML = i;
+    } else if (num.target.matches(".button-decrease")){
+        i--;
+        if (i<0){
+            number.style.color="#FF0000";
+        } else if (i == 0){
+            number.style.color="#000000";
+        };
+        number.innerHTML = i;
+    };
 });
